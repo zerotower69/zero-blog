287ea1f0e9ae282d41b102bc77d6c4b8c8845598
zerotower69 <zerotower@163.com>
2022-02-19 08:30:49 +0800

    emoji图片扩展名改为jpeg

--- web-vue/src/assets/js/emoji.js
+++ web-vue/src/assets/js/emoji.js
@@ -1,115 +1,115 @@
 export default {
-  "[微笑]": "http://static.wugenquan.cn/emoji/smile.jpg",
+  "[微笑]": "http://static.wugenquan.cn/emoji/smile.jpeg",
 
-  "[笑]": " http://static.wugenquan.cn/emoji/dx.jpg",
+  "[笑]": " http://static.wugenquan.cn/emoji/dx.jpeg",
 
-  "[呲牙]": "http://static.wugenquan.cn/emoji/cy.jpg",
+  "[呲牙]": "http://static.wugenquan.cn/emoji/cy.jpeg",
 
-  "[OK]": "http://static.wugenquan.cn/emoji/ok.jpg",
+  "[OK]": "http://static.wugenquan.cn/emoji/ok.jpeg",
 
-  "[星星眼]": "http://static.wugenquan.cn/emoji/xxy.jpg",
+  "[星星眼]": "http://static.wugenquan.cn/emoji/xxy.jpeg",
 
-  "[哦呼]": "http://static.wugenquan.cn/emoji/oh.jpg",
+  "[哦呼]": "http://static.wugenquan.cn/emoji/oh.jpeg",
 
-  "[嫌弃]": "http://static.wugenquan.cn/emoji/xq.jpg",
+  "[嫌弃]": "http://static.wugenquan.cn/emoji/xq.jpeg",
 
-  "[喜欢]": "http://static.wugenquan.cn/emoji/xh.jpg",
+  "[喜欢]": "http://static.wugenquan.cn/emoji/xh.jpeg",
 
-  "[酸了]": "http://static.wugenquan.cn/emoji/sl.jpg",
+  "[酸了]": "http://static.wugenquan.cn/emoji/sl.jpeg",
 
-  "[大哭]": "http://static.wugenquan.cn/emoji/dq.jpg?",
+  "[大哭]": "http://static.wugenquan.cn/emoji/dq.jpeg?",
 
-  "[害羞]": "http://static.wugenquan.cn/emoji/hx.jpg",
+  "[害羞]": "http://static.wugenquan.cn/emoji/hx.jpeg",
 
-  "[无语]": "http://static.wugenquan.cn/emoji/wy.jpg",
+  "[无语]": "http://static.wugenquan.cn/emoji/wy.jpeg",
 
-  "[疑惑]": "http://static.wugenquan.cn/emoji/yh.jpg",
+  "[疑惑]": "http://static.wugenquan.cn/emoji/yh.jpeg",
 
-  "[调皮]": "http://static.wugenquan.cn/emoji/tiaopi.jpg",
+  "[调皮]": "http://static.wugenquan.cn/emoji/tiaopi.jpeg",
 
-  "[笑哭]": "http://static.wugenquan.cn/emoji/xiaoku.jpg",
+  "[笑哭]": "http://static.wugenquan.cn/emoji/xiaoku.jpeg",
 
-  "[奸笑]": "http://static.wugenquan.cn/emoji/jianxiao.jpg",
+  "[奸笑]": "http://static.wugenquan.cn/emoji/jianxiao.jpeg",
 
-  "[偷笑]": "http://static.wugenquan.cn/emoji/touxiao.jpg",
+  "[偷笑]": "http://static.wugenquan.cn/emoji/touxiao.jpeg",
 
-  "[大笑]": "http://static.wugenquan.cn/emoji/daxiao.jpg",
+  "[大笑]": "http://static.wugenquan.cn/emoji/daxiao.jpeg",
 
-  "[阴险]": "http://static.wugenquan.cn/emoji/yinxian.jpg",
+  "[阴险]": "http://static.wugenquan.cn/emoji/yinxian.jpeg",
 
-  "[捂脸]": "http://static.wugenquan.cn/emoji/wulian.jpg",
+  "[捂脸]": "http://static.wugenquan.cn/emoji/wulian.jpeg",
 
-  "[呆]": "http://static.wugenquan.cn/emoji/dai.jpg",
+  "[呆]": "http://static.wugenquan.cn/emoji/dai.jpeg",
 
-  "[抠鼻]": "http://static.wugenquan.cn/emoji/koubi.jpg",
+  "[抠鼻]": "http://static.wugenquan.cn/emoji/koubi.jpeg",
 
-  "[惊喜]": "http://static.wugenquan.cn/emoji/jingxi.jpg",
+  "[惊喜]": "http://static.wugenquan.cn/emoji/jingxi.jpeg",
 
-  "[惊讶]": "http://static.wugenquan.cn/emoji/jingya.jpg",
+  "[惊讶]": "http://static.wugenquan.cn/emoji/jingya.jpeg",
 
-  "[捂脸哭]": "http://static.wugenquan.cn/emoji/wulianku.jpg",
+  "[捂脸哭]": "http://static.wugenquan.cn/emoji/wulianku.jpeg",
 
-  "[妙啊]": "http://static.wugenquan.cn/emoji/miaoa.jpg",
+  "[妙啊]": "http://static.wugenquan.cn/emoji/miaoa.jpeg",
 
-  "[狗头]": "http://static.wugenquan.cn/emoji/goutou.jpg",
+  "[狗头]": "http://static.wugenquan.cn/emoji/goutou.jpeg",
 
-  "[滑稽]": "http://static.wugenquan.cn/emoji/huaji.jpg",
+  "[滑稽]": "http://static.wugenquan.cn/emoji/huaji.jpeg",
 
-  "[吃瓜]": "http://static.wugenquan.cn/emoji/chigua.jpg",
+  "[吃瓜]": "http://static.wugenquan.cn/emoji/chigua.jpeg",
 
-  "[打call]": "http://static.wugenquan.cn/emoji/dacall.jpg",
+  "[打call]": "http://static.wugenquan.cn/emoji/dacall.jpeg",
 
-  "[点赞]": "http://static.wugenquan.cn/emoji/dianzan.jpg",
+  "[点赞]": "http://static.wugenquan.cn/emoji/dianzan.jpeg",
 
-  "[鼓掌]": "http://static.wugenquan.cn/emoji/guzhang.jpg",
+  "[鼓掌]": "http://static.wugenquan.cn/emoji/guzhang.jpeg",
 
-  "[尴尬]": "http://static.wugenquan.cn/emoji/ganga.jpg",
+  "[尴尬]": "http://static.wugenquan.cn/emoji/ganga.jpeg",
 
-  "[冷]": "http://static.wugenquan.cn/emoji/leng.jpg",
+  "[冷]": "http://static.wugenquan.cn/emoji/leng.jpeg",
 
-  "[灵魂出窍]": "http://static.wugenquan.cn/emoji/linghunchuqiao.jpg",
+  "[灵魂出窍]": "http://static.wugenquan.cn/emoji/linghunchuqiao.jpeg",
 
-  "[委屈]": "http://static.wugenquan.cn/emoji/weiqu.jpg",
+  "[委屈]": "http://static.wugenquan.cn/emoji/weiqu.jpeg",
 
-  "[傲娇]": "http://static.wugenquan.cn/emoji/aojiao.jpg",
+  "[傲娇]": "http://static.wugenquan.cn/emoji/aojiao.jpeg",
 
-  "[疼]": "http://static.wugenquan.cn/emoji/teng.jpg",
+  "[疼]": "http://static.wugenquan.cn/emoji/teng.jpeg",
 
-  "[吓]": "http://static.wugenquan.cn/emoji/xia.jpg?",
+  "[吓]": "http://static.wugenquan.cn/emoji/xia.jpeg?",
 
-  "[生病]": "http://static.wugenquan.cn/emoji/shengbing.jpg",
+  "[生病]": "http://static.wugenquan.cn/emoji/shengbing.jpeg",
 
-  "[吐]": "http://static.wugenquan.cn/emoji/tu.jpg",
+  "[吐]": "http://static.wugenquan.cn/emoji/tu.jpeg",
 
-  "[嘘声]": "http://static.wugenquan.cn/emoji/xusheng.jpg",
+  "[嘘声]": "http://static.wugenquan.cn/emoji/xusheng.jpeg",
 
-  "[捂眼]": "http://static.wugenquan.cn/emoji/wuyan.jpg",
+  "[捂眼]": "http://static.wugenquan.cn/emoji/wuyan.jpeg",
 
-  "[思考]": "http://static.wugenquan.cn/emoji/sikao.jpg",
+  "[思考]": "http://static.wugenquan.cn/emoji/sikao.jpeg",
 
-  "[再见]": "http://static.wugenquan.cn/emoji/zaijian.jpg",
+  "[再见]": "http://static.wugenquan.cn/emoji/zaijian.jpeg",
 
-  "[翻白眼]": "http://static.wugenquan.cn/emoji/fanbaiyan.jpg",
+  "[翻白眼]": "http://static.wugenquan.cn/emoji/fanbaiyan.jpeg",
 
-  "[哈欠]": "http://static.wugenquan.cn/emoji/haqian.jpg",
+  "[哈欠]": "http://static.wugenquan.cn/emoji/haqian.jpeg",
 
-  "[奋斗]": "http://static.wugenquan.cn/emoji/fengdou.jpg",
+  "[奋斗]": "http://static.wugenquan.cn/emoji/fengdou.jpeg",
 
-  "[墨镜]": "http://static.wugenquan.cn/emoji/mojing.jpg",
+  "[墨镜]": "http://static.wugenquan.cn/emoji/mojing.jpeg",
 
-  "[撇嘴]": "http://static.wugenquan.cn/emoji/piezui.jpg",
+  "[撇嘴]": "http://static.wugenquan.cn/emoji/piezui.jpeg",
 
-  "[难过]": "http://static.wugenquan.cn/emoji/nanguo.jpg",
+  "[难过]": "http://static.wugenquan.cn/emoji/nanguo.jpeg",
 
-  "[抓狂]": "http://static.wugenquan.cn/emoji/zhuakuang.jpg",
+  "[抓狂]": "http://static.wugenquan.cn/emoji/zhuakuang.jpeg",
 
-  "[生气]": "http://static.wugenquan.cn/emoji/shengqi.jpg",
+  "[生气]": "http://static.wugenquan.cn/emoji/shengqi.jpeg",
 
-  "[爱心]": "http://static.wugenquan.cn/emoji/aixin.jpg",
+  "[爱心]": "http://static.wugenquan.cn/emoji/aixin.jpeg",
 
-  "[胜利]": "http://static.wugenquan.cn/emoji/shengli.jpg",
+  "[胜利]": "http://static.wugenquan.cn/emoji/shengli.jpeg",
 
-  "[保佑]": "http://static.wugenquan.cn/emoji/baoyou.jpg",
+  "[保佑]": "http://static.wugenquan.cn/emoji/baoyou.jpeg",
 
-  "[支持]": "http://static.wugenquan.cn/emoji/zhichi.jpg"
+  "[支持]": "http://static.wugenquan.cn/emoji/zhichi.jpeg"
 };
